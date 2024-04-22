58aa3d8ba9f28ef73e52f037f34db92d10eb40a4
TANMAYAGARWALLSKT <131980930+TANMAYAGARWALLSKT@users.noreply.github.com>
2024-04-23 02:21:17 +0530

    nextui input form v.1

--- src/Components/Income/Incometable.jsx
+++ src/Components/Income/Incometable.jsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { addDoc, collection, getDocs } from "firebase/firestore";
 import { db } from "../../Config/firebase";
-import { useToast } from "@chakra-ui/react";
-import { Input } from "@chakra-ui/react";
-
-import { SparklesCore } from "../ui/sparkles";
+import { Input } from "@nextui-org/react";
 
 function Incometable() {
   // const Income = collection(db, "movies");
@@ -56,99 +53,98 @@ function Incometable() {
       console.error(error);
     }
   };
-  const toast = useToast();
+  // const toast = useToast();
   return (
-    <>
-      <div className=" pl-[6.5vw] mt-[5vh] flex flex-wrap  overflow-hidden  ml-[10vw] w-[85vw] h-[80vh] rounded-3xl Forst z-10 absolute top-0  text-zinc-100 ">
+    <div className="flex flex-wrap justify-center">
+      <div className="  flex flex-wrap  overflow-hidden  w-[80vw] h-[80vh] rounded-3xl Forst   text-zinc-100 ">
         <h1 className="font-bold text-green-600 text-[8vw] w-screen flex justify-center ">
           Add {type}
         </h1>
         <form
           id="Income"
           onSubmit={handleInput}
-          className=" flex flex-wrap p-20 pb-32 justify-between place-items-center  "
+          className=" flex flex-wrap  justify-between place-items-center  "
         >
-          <div className=" w-">
-            <label className=" text-6xl p-5 flex justify-center ">Model</label>
+          {/* <label className=" text-6xl p-5 ">Model</label>
             <Input
+            label ="model"
               size="lg"
               type="text"
               className="rounded-xl w-[20vw] p-5  bg-zinc-900/40 text-black text-5xl"
               placeholder="Enter Model Name"
-              onChange={(e) => SetModel(e.target.value)}
-            />
-          </div>
-          <div className="">
-            <label className="text-6xl p-5 flex justify-center  ">
-              Payment Mode
-            </label>
-            <Input
-              size="lg"
-              type="text"
-              id="payment-mode"
-              name="payment_mode"
-              className="rounded-xl bg-zinc-900   w-[20vw] p-5 text-black text-4xl"
-              placeholder="Enter Payment Mode"
-              list="payment-options"
-              onChange={(e) => SetPaymentMode(e.target.value)}
-            />
-            <datalist id="payment-options">
-              <option value="UPI">UPI</option>
-              <option value="Credit Card">C.C</option>
-              <option value="Cash">Cash</option>
-              <option value="Due">DUE</option>
-              <option value="Paid">Paid</option>
-              <option value="Bajaj">Bajaj</option>
-              <option value="Other">Others</option>
-            </datalist>
-          </div>
-          <div className="">
-            <label className="text-6xl p-5 flex justify-center ">Company</label>
-            <Input
-              size="lg"
-              type="text"
-              id="company"
-              name="company"
-              className="rounded-xl w-[20vw] bg-zinc-900/40 p-5 text-black text-4xl"
-              placeholder="Enter Phone Company"
-              list="phone-companies"
-              onChange={(e) => SetCompany(e.target.value)}
-            />
-            <datalist id="phone-companies">
-              <option value="Apple">Apple</option>
-              <option value="Samsung">Samsung</option>
-              <option value="Realme">Realme</option>
-              <option value="Vivo">Vivo</option>
-              <option value="OnePlus">OnePlus</option>
-              <option value="Mi">Mi</option>
-              <option value="Nokia">Nokia</option>
-              <option value="Oppo">Oppo</option>
-              <option value="Other">Other</option>
-            </datalist>
-          </div>
+              
+            /> */}
+          <Input
+            variant="bordered"
+            type="Mdel"
+            onChange={(e) => SetModel(e.target.value)}
+            label="Model"
+            placeholder="Enter your Model"
+            size="lg"
+            className="bg-zince-950"
+          />
+          <Input
+            label=" Payment Mode"
+            variant="bordered"
+            size="lg"
+            type="text"
+            id="payment-mode"
+            name="payment_mode"
+            placeholder="Enter Payment Mode"
+            list="payment-options"
+            onChange={(e) => SetPaymentMode(e.target.value)}
+          />
+          <Input
+            variant="bordered"
+            label=" Company Name"
+            size="lg"
+            type="text"
+            id="company"
+            name="company"
+            // className="rounded-xl w-[20vw] bg-zinc-900/40 p-5 text-black text-4xl"
+            placeholder="Enter Phone Company"
+            list="phone-companies"
+            onChange={(e) => SetCompany(e.target.value)}
+          />
+          <Input
+            variant="bordered"
+            size="lg"
+            label=" Amount"
+            type="number"
+            id="Amount"
+            name="Amount"
+            placeholder="Amount"
+            onChange={(e) => SetAmount(Number(e.target.value))}
+          />
+          <Input
+            label=" Note"
+            variant="bordered"
+            size="lg"
+            type="text"
+            placeholder="Note"
+            onChange={(e) => SetNotes(e.target.value)}
+          />
+          <datalist id="payment-options">
+            <option value="UPI">UPI</option>
+            <option value="Credit Card">C.C</option>
+            <option value="Cash">Cash</option>
+            <option value="Due">DUE</option>
+            <option value="Paid">Paid</option>
+            <option value="Bajaj">Bajaj</option>
+            <option value="Other">Others</option>
+          </datalist>
+          <datalist id="phone-companies">
+            <option value="Apple">Apple</option>
+            <option value="Samsung">Samsung</option>
+            <option value="Realme">Realme</option>
+            <option value="Vivo">Vivo</option>
+            <option value="OnePlus">OnePlus</option>
+            <option value="Mi">Mi</option>
+            <option value="Nokia">Nokia</option>
+            <option value="Oppo">Oppo</option>
+            <option value="Other">Other</option>
+          </datalist>
 
-          <div className="">
-            <label className="text-6xl p-5 flex justify-center ">Amount</label>
-            <Input
-              size="lg"
-              type="number"
-              id="Amount"
-              name="Amount"
-              className="rounded-xl w-[20vw] bg-zinc-900/40 p-5 text-black text-4xl"
-              placeholder="Amount"
-              onChange={(e) => SetAmount(Number(e.target.value))}
-            />
-          </div>
-          <div className="">
-            <label className="text-6xl p-5 flex justify-center  ">Note</label>
-            <Input
-              size="lg"
-              type="text"
-              className="rounded-xl w-[20vw] bg-zinc-900/40 p-5 text-black text-5xl"
-              placeholder="Note"
-              onChange={(e) => SetNotes(e.target.value)}
-            />
-          </div>
           <div className="text-6xl p-5 flex justify-center ">
             <button
               className="text-7xl  bg-green-500/40 w-[20vw] h-[10vh] mt-[7rem]   rounded-full"
@@ -167,7 +163,7 @@ function Incometable() {
           </div>
         </form>
       </div>
-    </>
+    </div>
   );
 }
 
